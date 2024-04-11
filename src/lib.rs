22853b961f82914f950f85076bebb5061ea3b5c0
Wolf McNally <wolf@wolfmcnally.com>
2024-04-10 23:43:47 -0700

    Aligned to dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-components/0.10.0")]
+#![doc(html_root_url = "https://docs.rs/bc-components/0.10.1")]
 #![warn(rust_2018_idioms)]
 
 //! # Introduction
@@ -16,7 +16,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-components = "0.10.0"
+//! bc-components = "0.10.1"
 //! ```
 
 mod digest;
