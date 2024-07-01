0a0913d14fd3b90188663464c027627bdf4ae21d
Wolf McNally <wolf@wolfmcnally.com>
2024-07-01 00:36:43 -0700

    Bumped version number.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-components/0.11.0")]
+#![doc(html_root_url = "https://docs.rs/bc-components/0.12.0")]
 #![warn(rust_2018_idioms)]
 
 //! # Introduction
@@ -16,7 +16,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-components = "0.11.0"
+//! bc-components = "0.12.0"
 //! ```
 
 mod digest;
